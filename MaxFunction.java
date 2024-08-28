7779ad40a03a2f85e182c0226037bce00986c245
Isha Shinde <ishashinde2021@gmail.com>
2024-08-28 07:00:02 +0530

    Added: Extend the max Method to take more than # parameter

--- MaxFunction.java
+++ MaxFunction.java
@@ -2,36 +2,36 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the application!");
 
-        Compare<Integer> integerCompare = new Compare<>(10, 15, 20);
+        Compare<Integer> integerCompare = new Compare<>(10, 15, 20, 25, 5);
         System.out.println("Maximum of integers: " + integerCompare.testMaximum());
 
-        Compare<Double> doubleCompare = new Compare<>(3.5, 2.5, 4.1);
+        Compare<Double> doubleCompare = new Compare<>(3.5, 2.5, 4.1, 5.2, 1.8);
         System.out.println("Maximum of doubles: " + doubleCompare.testMaximum());
     }
 }
 
 class Compare<T extends Comparable<T>> {
-    private T a;
-    private T b;
-    private T c;
+    private T[] values;
 
-    Compare(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    @SafeVarargs
+    Compare(T... values) {
+        this.values = values;
     }
 
     public T testMaximum() {
-        return Compare.testMaximum(a, b, c);
+        return Compare.testMaximum(values);
     }
 
-    public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
-        T max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
+    public static <T extends Comparable<T>> T testMaximum(T... values) {
+        if (values.length == 0) {
+            throw new IllegalArgumentException("At least one value must be provided");
         }
-        if (c.compareTo(max) > 0) {
-            max = c;
+
+        T max = values[0];
+        for (T value : values) {
+            if (value.compareTo(max) > 0) {
+                max = value;
+            }
         }
         return max;
     }
