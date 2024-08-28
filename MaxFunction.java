b8a6057e08cb196b514b6408da62fb9bda2da92b
Isha Shinde <ishashinde2021@gmail.com>
2024-08-28 06:39:56 +0530

    Added: Max function By using String of three Variables

--- MaxFunction.java
+++ MaxFunction.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to application");
+
+        System.out.println(maxint(10,25,12));
         System.out.println(maxFloat(10.2f,25.2f,30.1f));
+        System.out.println(maxString("Apple", "Peach" , "Banana"));
+    }
+    static int maxint(int a , int b , int c){
+        int max = 0;
+        max = Math.max(a , b);
+        max = Math.max(max , c);
+
+        return max;
+
+
     }
 
     static float maxFloat(float a, float b , float c){
@@ -14,5 +26,18 @@ public class Main {
         else {
             return c;
         }
+
+    }
+    static String maxString(String a , String b, String c){
+        String max = a;
+
+        if(b.compareTo(max) > 0){
+            max = b;
+        }
+        else if(c.compareTo(max) > 0){
+            max = c;
+        }
+
+        return max;
     }
 }
