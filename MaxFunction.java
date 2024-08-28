4895ae04ddf03f51dfcc54d7066bb1d7573448a8
Isha Shinde <ishashinde2021@gmail.com>
2024-08-28 06:26:21 +0530

    Added: Maximum of Three By using Integer

--- MaxFunction.java
+++ MaxFunction.java
@@ -1,9 +1,13 @@
-import java.util.*;
+public class MaxFunction {
+    public static void main(String[] args) {
+        System.out.println("Welcome to application!");
+        System.out.println(maxint(10,25,12));
+    }
+    static int maxint(int a , int b , int c){
+        int max = 0;
+        max = Math.max(a , b);
+        max = Math.max(max , c);
 
-class MaxFunctin
-{
-    public static void main(String arg[])
-    {
-        System.out.println("Welcome To maxfunction")
+        return max;
     }
 }
